439c53e1c81401a73c86e6187c5d6e64f7b6f87b
Sergio Silva <sergiobtos@hotmail.com>
2023-06-13 22:52:19 -0700

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+    backgroundColor: '$green500',
+    borderRadius: 4,
+    border: 0,
+    padding: '4px 8px',
+})
+
+export default function Home() {
+    return (
+        <Button>Hello World</Button>
+    )
+}
