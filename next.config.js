1fff0fdde1c77898cbc12fcf6f1eca76b7f520d7
Sergio Silva <sergiobtos@hotmail.com>
2023-06-22 20:34:04 -0700

    Link Navigation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: [
+            'files.stripe.com',
+        ]
+    },
+}
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,76 @@ import Head from "next/head"
 import Link from "next/link"
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
+import { stripe } from "../lib/stripe"
+import Stripe from "stripe"
 
-export default function Home() {
+interface HomeProps {
+    products: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: number;
+    }[]
+}
+
+export default function Home({products}: HomeProps) {
+    const [sliderRef] = useKeenSlider({
+        slides: {
+            perView: 3,
+            spacing: 48,
+        }
+    })
+
     return (
         <>
             <Head>
                 <title>Home | Ignite Shop</title>
             </Head>
-            <HomeContainer>
-                <Product className="keen-slider__slide">
-                    <Image src='' width={520} height={480} alt="" />
-                    <footer>
-                        <strong>product.name</strong>
-                        <span>product.price</span>
-                    </footer>
-                </Product>
+           <HomeContainer ref={sliderRef} className="keen-slider">
+               {products.map(product => {
+                return (
+                    <Link href={`/product/${product.id}`} key={product.id}>
+                        <Product className="keen-slider__slide">
+                        <Image src={product.imageUrl} width={520} height={480} alt="1" />
+                        <footer>
+                            <strong>{product.name}</strong>
+                            <span>{product.price}</span>
+                        </footer>
+                        </Product>
+                    </Link>
+                )
+               })}
             </HomeContainer>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    //await new Promise(resolve => setTimeout(resolve, 1000));
+    const response = await stripe.products.list({
+        expand: ['data.default_price']
+    });
+    const responseData = response.data.filter(product => product.name !== 'Subscription')
+
+    const products = responseData.map(product => {
+        const price = product.default_price as Stripe.Price;
+        if(product.name !== 'Subscription') {
+            return {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0] || '',
+                price: new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                }).format(price.unit_amount! / 100),
+            }
+        }   
+    })
+
+    return {
+        props: {
+            products,
+        },
+        revalidate: 60 * 60 * 2 , // 2 hours
+    }
 }
